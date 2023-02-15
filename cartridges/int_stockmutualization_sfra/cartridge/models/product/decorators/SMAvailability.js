3c7513f41249fd8efef26f25f5f94643f8509828
balcantara <bruno.alcantara95@hotmail.com>
2023-02-14 22:05:23 -0300

    Wrapping up SM Availability Attribute on the Product

--- cartridges/int_stockmutualization_sfra/cartridge/models/product/decorators/SMAvailability.js
+++ cartridges/int_stockmutualization_sfra/cartridge/models/product/decorators/SMAvailability.js
@@ -1,89 +1,99 @@
 'use strict';
 
 var Site = require('dw/system/Site');
+var Resource = require('dw/web/Resource');
+
+var ProductInventoryMgr = require('dw/catalog/ProductInventoryMgr');
 
 module.exports = function (object, apiProduct, quantity, minOrderQuantity, availabilityModel) {
     var stockMutualizationEnabled = Site.current.getCustomPreferenceValue('SM_Enabled');
     var SMInventoryListID = Site.current.getCustomPreferenceValue('SM_InventoryID');
-    if (stockMutualizationEnabled && !empty(inventoryListID)) {
+    if (stockMutualizationEnabled && !empty(SMInventoryListID)) {
+        var availability = {};
+        availability.messages = [];
+        var productQuantity = quantity ? parseInt(quantity, 10) : minOrderQuantity;
+        var SMInventoryList = ProductInventoryMgr.getInventoryList(SMInventoryListID);
+        var SMAvailabilityModel = apiProduct.getAvailabilityModel(SMInventoryList);
+        var availabilityModelLevels = availabilityModel.getAvailabilityLevels(productQuantity);
+        var SMAvailabilityModelLevels = SMAvailabilityModel.getAvailabilityLevels(productQuantity);
+        var inventoryRecord = availabilityModel.inventoryRecord;
+        var SMInventoryRecord = SMAvailabilityModel.inventoryRecord;
+
+        if (inventoryRecord && inventoryRecord.inStockDate) {
+            availability.inStockDate = inventoryRecord.inStockDate.toDateString();
+        } else if (SMInventoryRecord && SMInventoryRecord.inStockDate) {
+            availability.inStockDate = SMInventoryRecord.inStockDate.toDateString();
+        } else {
+            availability.inStockDate = null;
+        }
+
+        var inStockValue = availabilityModelLevels.inStock.value + SMAvailabilityModelLevels.inStock.value;
+        var preOrderValue = availabilityModelLevels.preorder.value + SMAvailabilityModelLevels.preorder.value;
+        var backOrderValue = availabilityModelLevels.backorder.value + SMAvailabilityModelLevels.backorder.value;
+        var notAvailableValue = productQuantity - (inStockValue + preOrderValue + backOrderValue);
+
+        if (inStockValue > 0) {
+            if (inStockValue >= productQuantity) {
+                availability.messages.push(Resource.msg('label.instock', 'common', null));
+            } else {
+                availability.messages.push(
+                    Resource.msgf(
+                        'label.quantity.in.stock',
+                        'common',
+                        null,
+                        inStockValue
+                    )
+                );
+            }
+        }
+
+        if (preOrderValue > 0) {
+            if (preOrderValue === productQuantity) {
+                availability.messages.push(Resource.msg('label.preorder', 'common', null));
+            } else {
+                availability.messages.push(
+                    Resource.msgf(
+                        'label.preorder.items',
+                        'common',
+                        null,
+                        preOrderValue
+                    )
+                );
+            }
+        }
+
+        if (backOrderValue > 0) {
+            if (backOrderValue === productQuantity) {
+                availability.messages.push(Resource.msg('label.back.order', 'common', null));
+            } else {
+                availability.messages.push(
+                    Resource.msgf(
+                        'label.back.order.items',
+                        'common',
+                        null,
+                        backOrderValue
+                    )
+                );
+            }
+        }
+
+        if (notAvailableValue > 0) {
+            if (notAvailableValue === productQuantity) {
+                availability.messages.push(Resource.msg('label.not.available', 'common', null));
+            } else {
+                availability.messages.push(Resource.msg('label.not.available.items', 'common', null));
+            }
+        }
+
+        var isOrderable = availabilityModel.isOrderable(parseFloat(quantity - SMAvailabilityModelLevels.inStock.value) || minOrderQuantity) && SMAvailabilityModel.isOrderable(parseFloat(quantity - availabilityModelLevels.inStock.value) || minOrderQuantity);
+
         Object.defineProperty(object, 'SMAvailability', {
             enumerable: true,
-            value: (function () {
-                var availability = {};
-                availability.messages = [];
-                var productQuantity = quantity ? parseInt(quantity, 10) : minOrderQuantity;
-                var SMInventoryList = ProductInventoryMgr.getInventoryList(SMInventoryListID);
-                var SMAvailabilityModel = apiProduct.getAvailabilityModel(SMInventoryList);
-                var availabilityModelLevels = availabilityModel.getAvailabilityLevels(productQuantity);
-                var SMAvailabilityModelLevels = SMAvailabilityModel.getAvailabilityLevels(productQuantity);
-                var inventoryRecord = availabilityModel.inventoryRecord;
-                var SMInventoryRecord = SMAvailabilityModel.inventoryRecord;
-
-                if (inventoryRecord && inventoryRecord.inStockDate) {
-                    availability.inStockDate = inventoryRecord.inStockDate.toDateString();
-                } else {
-                    availability.inStockDate = null;
-                }
-
-                if (availabilityModelLevels.inStock.value > 0) {
-                    if (availabilityModelLevels.inStock.value === productQuantity) {
-                        availability.messages.push(Resource.msg('label.instock', 'common', null));
-                    } else {
-                        availability.messages.push(
-                            Resource.msgf(
-                                'label.quantity.in.stock',
-                                'common',
-                                null,
-                                availabilityModelLevels.inStock.value
-                            )
-                        );
-                    }
-                }
-
-                if (availabilityModelLevels.preorder.value > 0) {
-                    if (availabilityModelLevels.preorder.value === productQuantity) {
-                        availability.messages.push(Resource.msg('label.preorder', 'common', null));
-                    } else {
-                        availability.messages.push(
-                            Resource.msgf(
-                                'label.preorder.items',
-                                'common',
-                                null,
-                                availabilityModelLevels.preorder.value
-                            )
-                        );
-                    }
-                }
-
-                if (availabilityModelLevels.backorder.value > 0) {
-                    if (availabilityModelLevels.backorder.value === productQuantity) {
-                        availability.messages.push(Resource.msg('label.back.order', 'common', null));
-                    } else {
-                        availability.messages.push(
-                            Resource.msgf(
-                                'label.back.order.items',
-                                'common',
-                                null,
-                                availabilityModelLevels.backorder.value
-                            )
-                        );
-                    }
-                }
-
-                if (availabilityModelLevels.notAvailable.value > 0) {
-                    if (availabilityModelLevels.notAvailable.value === productQuantity) {
-                        availability.messages.push(Resource.msg('label.not.available', 'common', null));
-                    } else {
-                        availability.messages.push(Resource.msg('label.not.available.items', 'common', null));
-                    }
-                }
-
-                return availability;
-            }())
+            value: availability
         });
         Object.defineProperty(object, 'SMAvailable', {
             enumerable: true,
-            value: availabilityModel.isOrderable(parseFloat(quantity) || minOrderQuantity)
+            value: isOrderable
         });
     }
 }
